0df8ca0d4aa9c21c495c15d3ddb5c2bd6f929624
Abrar Khan <heyabrarkhan@gmail.com>
2023-01-24 15:46:29 +0530

    set

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config( );
 
 const connection = mongoose.connect(process.env.MONGO_URL);
+mongoose.set('strictQuery', false);
 
 module.exports = {
     connection
